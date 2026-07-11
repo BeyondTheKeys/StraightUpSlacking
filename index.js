41d592fb8f1b4066f09044ef52e9ee16b65984fe
BeyondTheKeys <93868542+BeyondTheKeys@users.noreply.github.com>
2026-07-10 19:05:16 -0600

    basic starting features

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const axios = require("axios");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/sts-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/sts-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/sts-ping - Check bot latency
+/sts-catfact - Get a cat fact`
+  });
+});
+app.command("/sts-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
